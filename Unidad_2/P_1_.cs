1f40c989b2d0f23cbca4bf577b5e6434dc47f76c
dollycheri <146400981+dollycheri@users.noreply.github.com>
2023-09-29 15:07:51 -0600

    Create P_1_.cs

---
+++ Unidad_2/P_1_.cs
@@ -0,0 +1,21 @@
+﻿//Comentarios en una linea
+/*Comentarios en bloque:
+ * Console en Writeline para imprimir
+ * Para imprimir texto sin variable
+ * tiene que ir entre "" comillas dobles*/
+//Cada linea debe terminar con ; punto y coma
+using System; // Paso 1 
+namespace P_1 //Paso 1
+{
+    internal class Program //Paso 1
+    {
+        static void Main(string[] args) //Paso 1
+        {
+            // DECLARAR VARIABLES
+            string nombre = ("Frida Cisneros");
+            //Imprimir Nombre referenciado a una variable
+            Console.WriteLine(nombre);
+            //Cada linea debe terminar con ; punto y coma
+        }
+    }
+}
