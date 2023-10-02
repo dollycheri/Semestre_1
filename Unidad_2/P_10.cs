cfce45de677d850f8ae39d21e4b6bef39be77aab
dollycheri <146400981+dollycheri@users.noreply.github.com>
2023-10-01 21:39:56 -0600

    Add files via upload

---
+++ Unidad_2/P_10.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace P_10
+{
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lado: ");
+            double lado = double.Parse(Console.ReadLine());
+            double perimetro = lado * 6;
+            Console.WriteLine($"{perimetro} cm");
+            Console.ReadKey();
+        }
+    }
+}
