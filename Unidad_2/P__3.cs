f6a5abf70f2a47967b19010dc2681ea1b0c0e74c
dollycheri <146400981+dollycheri@users.noreply.github.com>
2023-10-01 19:23:37 -0600

    Add files via upload

---
+++ Unidad_2/P__3.cs
@@ -0,0 +1,34 @@
+﻿//Realizar un algoritmo que determine el promedio de 6 calificaciones
+using System;
+
+namespace P_3_
+{
+    internal class Promedio
+    {
+        static void Main(string[] args)
+        {
+            int c1, c2, c3, c4, c5, c6;
+            float promedio;
+
+            Console.WriteLine("Escribe calificacion 1: ");
+            c1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Escribe calificacion 2: ");
+            c2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Escribe calificacion 3: ");
+            c3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Escribe calificacion 4: ");
+            c4 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Escribe calificacion 5: ");
+            c5 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Escribe calificacion 6: ");
+            c6 = Convert.ToInt32(Console.ReadLine());
+            //PASO- CALCULA PROMEDIO
+            promedio = (c1 + c2 + c3 + c4 + c5 + c6) / 6;
+            //PASO- IMPRIME EL RESULTADO
+            Console.WriteLine("Tu promedio es: " + promedio);
+        }
+
+
+    }
+
+}
