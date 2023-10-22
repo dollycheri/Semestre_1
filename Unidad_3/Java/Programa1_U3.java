ed98cb984e0e21bd59f538a131920112156f8fa3
dollycheri <146400981+dollycheri@users.noreply.github.com>
2023-10-22 17:46:00 -0600

    Update Programa1_U3.java

--- Unidad_3/Java/Programa1_U3.java
+++ Unidad_3/Java/Programa1_U3.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.programa1_U3;
+package com.mycompany.Programa1_U3;
 
 import java.util.Scanner;
 
