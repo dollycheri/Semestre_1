4be786e52d551849c4681bed5e218cfbe42e0b11
dollycheri <146400981+dollycheri@users.noreply.github.com>
2023-10-22 18:31:40 -0600

    Update Programa2_U3.cs

--- Unidad_3/C#/Programa2_U3.cs
+++ Unidad_3/C#/Programa2_U3.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace MyApp // Note: actual namespace depends on the project name.
+namespace Programa2_U3 // Note: actual namespace depends on the project name.
 {
     internal class Programa2_U3
     {
