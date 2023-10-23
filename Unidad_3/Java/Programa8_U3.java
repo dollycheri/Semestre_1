c13308feb9d7ae2b590f73211d42429e239d1835
dollycheri <146400981+dollycheri@users.noreply.github.com>
2023-10-22 20:37:48 -0600

    Update Programa8_U3.java

--- Unidad_3/Java/Programa8_U3.java
+++ Unidad_3/Java/Programa8_U3.java
@@ -1,8 +1,8 @@
 
-package com.mycompany.programa8;
+package com.mycompany.Programa8_U3;
 import java.util.Scanner;
 
-public class programa8 {
+public class Programa8_U3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Ingrese el primer número: ");
