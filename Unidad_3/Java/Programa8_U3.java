59510ea4e24db6a5df2b8d73fb27893683b3d752
dollycheri <146400981+dollycheri@users.noreply.github.com>
2023-10-22 20:37:08 -0600

    Add files via upload

---
+++ Unidad_3/Java/Programa8_U3.java
@@ -0,0 +1,21 @@
+
+package com.mycompany.programa8;
+import java.util.Scanner;
+
+public class programa8 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese el primer número: ");
+        double numero1 = scanner.nextDouble();
+        System.out.print("Ingrese el segundo número: ");
+        double numero2 = scanner.nextDouble();
+
+        if (numero1 > numero2) {
+            System.out.println("El primer número es mayor.");
+        } else if (numero2 > numero1) {
+            System.out.println("El segundo número es mayor.");
+        } else {
+            System.out.println("Ambos números son iguales.");
+        }
+    }
+}
