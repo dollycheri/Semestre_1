cd0130d22cfca2d0934d6c3deaaf4aa7870359d6
dollycheri <146400981+dollycheri@users.noreply.github.com>
2023-10-22 20:24:26 -0600

    Update Programa6_U3.java

--- Unidad_3/Java/Programa6_U3.java
+++ Unidad_3/Java/Programa6_U3.java
@@ -1,8 +1,8 @@
 
-package com.mycompany.programa6;
+package com.mycompany.Programa6_U3;
 import java.util.Scanner;
 
-public class programa6 {
+public class Programa6_U3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Ingrese un número entero: ");
