b4626d1fc2157e04deac75231f4873ba67cadc92
dollycheri <146400981+dollycheri@users.noreply.github.com>
2023-10-22 18:33:52 -0600

    Add files via upload

---
+++ Unidad_3/C#/Programa3_U3.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Programa_3;// Note: actual namespace depends on the project name.
+
+    internal class Program
+    {       
+            public static void Main(String[] args)
+            {
+                
+                int dia;
+                Console.Write("Escribe el dia de la semana numerico ");
+                dia = Convert.ToInt32(Console.ReadLine());
+                switch (dia)
+                {
+                    case 1:
+                        Console.WriteLine("Lunes");
+                        break;
+                    case 2:
+                        Console.WriteLine("Martes");
+                        break;
+                    case 3:
+                        Console.WriteLine("Miercoles");
+                        break;
+                    case 4:
+                        Console.WriteLine("Jueves");
+                        break;
+                    case 5:
+                        Console.WriteLine("Viernes");
+                        break;
+                    case 6:
+                        Console.WriteLine("Sabado");
+                        break;
+                    case 7:
+                        Console.WriteLine("Domingo");
+                        break;
+                      default:
+                        Console.WriteLine("No existe el dia ingresado");
+                      break;
+        }
+            }
+}   
+
+
