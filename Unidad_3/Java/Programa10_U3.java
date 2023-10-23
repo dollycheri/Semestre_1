6ba91cbe221241341ac8ac270f77f268fed5bd8c
dollycheri <146400981+dollycheri@users.noreply.github.com>
2023-10-22 20:54:42 -0600

    Add files via upload

---
+++ Unidad_3/Java/Programa10_U3.java
@@ -0,0 +1,21 @@
+
+package com.mycompany.programa10;
+import java.util.Scanner;
+
+public class programa10 {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese las horas trabajadas: ");
+        double horasTrabajadas = scanner.nextDouble();
+        System.out.print("Ingrese el pago por hora: $");
+        double pagoPorHora = scanner.nextDouble();
+
+        double sueldo = horasTrabajadas * pagoPorHora;
+        if (horasTrabajadas > 40) {
+            double horasExtras = horasTrabajadas - 40;
+            double pagoHorasExtras = horasExtras * (pagoPorHora * 2);
+            sueldo += pagoHorasExtras;
+        }
+
+        System.out.println("El sueldo semanal es: $" + sueldo);
+    }
+}
