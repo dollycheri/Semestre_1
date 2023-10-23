4e214617b9a375a38441873d828b38f6b7423441
dollycheri <146400981+dollycheri@users.noreply.github.com>
2023-10-22 18:07:21 -0600

    Add files via upload

---
+++ Unidad_3/C#/Programa5_U3.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Proyecto5u3
+{
+    internal class Proyecto5u3
+    {
+
+        static void main(String[] args)
+        {
+
+            double p1, p2, p3, total;
+
+            Console.Write("Escribe el precio 1: ");
+            p1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Escribe el precio 2: ");
+            p2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Escribe el precio 3: ");
+            p3 = Convert.ToDouble(Console.ReadLine());
+            total = p1 + p2 + p3;
+            if (total >= 1500)
+            {
+                total = total - (total * 0.30);
+                Console.WriteLine("El total (30%desc): " + total);
+            }
+            else if (total < 1500 && total >= 1000)
+            {
+                total = total - (total * 0.20);
+                Console.WriteLine("El total (20%desc): " + total);
+            }
+            else if (total < 1000 && total >= 700)
+            {
+                total = total - (total * 0.10);
+                Console.WriteLine("El total (10%desc): " + total);
+            }
+            else
+            {
+                Console.WriteLine("El total (sin desc): " + total);
+            }
+        }
+    }
