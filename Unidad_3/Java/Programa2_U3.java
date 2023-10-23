782458cb6da3ab2ccf182341b1283370f2d6a24c
dollycheri <146400981+dollycheri@users.noreply.github.com>
2023-10-22 18:30:00 -0600

    Update Programa2_U3.java

--- Unidad_3/Java/Programa2_U3.java
+++ Unidad_3/Java/Programa2_U3.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.p2_u3;
+package com.mycompany.Programa2_U3;
 
 import java.util.Scanner;
 
@@ -10,7 +10,7 @@ import java.util.Scanner;
  *
  * @author LaboratorioLTI2
  */
-public class P2_U3 {
+public class Programa2_U3 {
 
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
