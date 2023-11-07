63102e4164226609fa98ec0851edc77a76284dd7
dollycheri <146400981+dollycheri@users.noreply.github.com>
2023-11-07 17:58:27 -0600

    Create Programa1_U4.cs
    Unidad 4

---
+++ Unidad_4/Programa1_U4.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MyCompany.BaseDeDatos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] edades = new int[] { 17, 19, 18, 18, 18, };
+            int[] edades2 = new int [] { 17, 19, 18, 18, 18 };
+
+            for (int i = 0; i < edades.Length; i++)
+            {
+                Console.WriteLine("indice: " + i + "es: " + edades[i]);
+                if (edades[i] >= 18)
+                    Console.WriteLine("Eres Mayor");
+                else
+                    Console.WriteLine("Eres Menor");
+            }
+
+        }
+    }
+}
