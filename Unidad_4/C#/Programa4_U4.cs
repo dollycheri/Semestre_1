fc71f5d17275eaea45d73cdf5d61fcda8259adf8
dollycheri <146400981+dollycheri@users.noreply.github.com>
2023-11-11 00:11:25 -0600

    .

---
+++ Unidad_4/C#/Programa4_U4.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+        int[] numeros = { 5, -2, 10, -8, 3, -1, 7 };
+
+       
+        int totalN = ContarN(numeros);
+
+        
+        Console.WriteLine($"El total de números negativos es: {totalN}");
+    }
+
+    static int ContarN(int[] array)
+    {
+        int contador = 0;
+
+        
+        foreach (int numero in array)
+        {
+            if (numero < 0)
+            {
+                contador++;
+            }
+        }
+
+        return contador;
+    }
+}
+
---
+++ Unidad_4/C#/Programa5_U4-.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+        string[] frutas = { "Manzana", "Fresa", "Cereza", "Frambuesa", "Granada" };
+
+        
+        string[] frutas_AlReves = new string[frutas.Length];
+
+       
+        for (int i = 0; i < frutas.Length; i++)
+        {
+            frutas_AlReves[i] = frutas[frutas.Length - 1 - i];
+        }
+
+       
+        Console.WriteLine("Arreglo Original:");
+        ImprimirArreglo(frutas);
+
+        
+        Console.WriteLine("\nArreglo al Revés:");
+        ImprimirArreglo(frutas_AlReves);
+    }
+
+    static void ImprimirArreglo(string[] array)
+    {
+        foreach (string elemento in array)
+        {
+            Console.Write($"{elemento} ");
+        }
+        Console.WriteLine();
+    }
+}
