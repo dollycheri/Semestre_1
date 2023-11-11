0f3740f2e83e114c44aa1518d54041f5df59fe1a
dollycheri <146400981+dollycheri@users.noreply.github.com>
2023-11-11 14:44:37 -0600

    Update Programa2_U4.java

--- Unidad_4/Java/Programa2_U4.java
+++ Unidad_4/Java/Programa2_U4.java
@@ -12,19 +12,18 @@ import java.util.Scanner;
 
 public class Programa2_U4 {
     public static void main(String[] args) {
-        // Declarar un arreglo para almacenar las calificaciones
-        double[] calificaciones = new double[10];
 
-        // Crear un objeto Scanner para leer la entrada del usuario
+        double[] calificaciones = new double[10];
+        
         Scanner sc = new Scanner(System.in);
 
-        // Pedir al usuario que ingrese las calificaciones
+     
         for (int i = 0; i < 10; i++) {
             System.out.print("Ingrese la calificación " + (i + 1) + ": ");
             calificaciones[i] = Double.parseDouble(sc.nextLine());
         }
 
-        // Calcular el promedio
+   
         double suma = 0;
         for (int i = 0; i < 10; i++) {
             suma += calificaciones[i];
@@ -32,10 +31,8 @@ public class Programa2_U4 {
 
         double promedio = suma / 10;
 
-        // Mostrar el promedio
         System.out.println("El promedio de las calificaciones es: " + promedio);
 
-        // Cerrar el objeto Scanner
         sc.close();
     }
 }
