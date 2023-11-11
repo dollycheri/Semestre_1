90f1baa6a6b288021a4ba567e5fa3dc7ed4307d2
dollycheri <146400981+dollycheri@users.noreply.github.com>
2023-11-11 15:27:15 -0600

    ,

---
+++ Unidad_4/Java/Programa9_U4.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.programa9_u4;
+
+/**
+ *
+ * @author leebc
+ */
+public class Programa9_U4 {
+    public static void main(String[] args) {
+        String[] nombres = { "Michel", "Angel", "Axel", "Ana" };
+        String[] apellidos = { "Cisneros", "Rocha", "Galvan", "Alvarez" };
+
+        String[] nombres_Completos = nombres_Apellidos(nombres, apellidos);
+
+        System.out.println("Nombres completos:");
+        imprimir_Arreglo(nombres_Completos);
+    }
+
+    static String[] nombres_Apellidos(String[] nombres, String[] apellidos) {
+        String[] nombres_Completos = new String[nombres.length];
+
+        for (int i = 0; i < nombres.length; i++) {
+            nombres_Completos[i] = nombres[i] + " " + apellidos[i];
+        }
+
+        return nombres_Completos;
+    }
+
+    static void imprimir_Arreglo(String[] arreglo) {
+        for (String elemento : arreglo) {
+            System.out.println(elemento);
+        }
+    }
+}
