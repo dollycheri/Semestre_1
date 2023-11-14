2fa063c45c9bcfcb3aae0bb97b458d3ff22be2d4
dollycheri <146400981+dollycheri@users.noreply.github.com>
2023-11-14 17:41:35 -0600

    .

---
+++ Unidad_4/C#/Programa12_u4.cs
@@ -0,0 +1,20 @@
+﻿
+public class Programa11_U4
+{
+
+    public static void Main(String[] args)
+    {
+
+        int[,] calf = { {5,7,9} , {6,8,7} , {10,3,1} };
+
+        Console.Write(calf[0,0] + ",");
+        Console.Write(calf[0,1] + ",");
+        Console.WriteLine(calf[0,2] + ",");
+        Console.Write(calf[1,0] + ",");
+        Console.Write(calf[1,1] + ",");
+        Console.WriteLine(calf[1,2] + ",");
+        Console.Write(calf[2,0] + ",");
+        Console.Write(calf[2,1] + ",");
+        Console.WriteLine(calf[2,2] + ",");
+    }
+}
---
+++ Unidad_4/Java/Programa11_U4.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.programa11_u4;
+
+/**
+ *
+ * @author LaboratorioLTI2
+ */
+public class Programa11_U4 {
+
+    public static void main(String[] args) {
+       /* int [][] calf = new int [3][3];
+        calf [0][0] = 5;
+        calf [0][1] = 7;
+        calf [0][2] = 9;
+        calf [1][0] = 6;
+        calf [1][1] = 8;
+        calf [1][2] = 7;
+        calf [2][0] = 10;
+        calf [2][1] = 3;
+        calf [2][2] = 1; */
+        
+        int [][] calf = { {5,7,9},
+                        {6,8,7},
+                        {10,3,1} };
+        
+        System.out.println(calf[0][0]);
+        System.out.println(calf[0][1]);
+        System.out.println(calf[0][2]);
+        System.out.println(calf[1][0]);
+        System.out.println(calf[1][0]);
+        System.out.println(calf[1][1]);
+        System.out.println(calf[1][2]);
+        System.out.println(calf[2][0]);
+        System.out.println(calf[2][1]);
+        System.out.println(calf[2][2]);
+    }
+}
