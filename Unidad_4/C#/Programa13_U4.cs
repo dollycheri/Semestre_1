26b29051640f6abe4385c2101e151835f40b2428
dollycheri <146400981+dollycheri@users.noreply.github.com>
2023-11-14 18:02:10 -0600

    .

---
+++ Unidad_4/C#/Programa13_U4.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Programa13_U4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] calf = { { 5, 7, 9, 5 }, { 6, 8, 7, 5 }, { 10, 3, 1, 5 } };
+
+            int fila = calf.GetLength(0);
+            int columna = calf.GetLength(0);
+            for (int f = 0; f <fila ; f++)
+            {
+                for (int c= 0; c <= columna; c++)
+                {
+                    Console.Write(calf[f, c] + ",");
+                }
+                Console.WriteLine();
+            }
+
+
+        }
+
+
+    }
+
+
+
+
+}
