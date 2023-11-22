1b617c344d76152df4e86d4405d2cd7489e28238
dollycheri <146400981+dollycheri@users.noreply.github.com>
2023-11-22 17:40:24 -0600

    .

---
+++ Unidad_4/Java/Programa20_U4.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.arreglomulti;
+
+/**
+ *
+ * @author LaboratorioLTI2
+ */
+public class ArregloMulti {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
