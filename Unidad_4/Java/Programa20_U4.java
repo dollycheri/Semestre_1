c1e8fa3092ddec2204dae3913511019add3c2cff
dollycheri <146400981+dollycheri@users.noreply.github.com>
2023-11-22 19:33:16 -0600

    Update Programa20_U4.java

--- Unidad_4/Java/Programa20_U4.java
+++ Unidad_4/Java/Programa20_U4.java
@@ -2,15 +2,27 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.arreglomulti;
+package com.mycompany.programa20_u4;
 
 /**
  *
  * @author LaboratorioLTI2
  */
-public class ArregloMulti {
+public class Programa20_U4 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int num[][] = { {5,6} , {8,9} };
+        int res [][] = new int [2][2];
+        for (int f=0; f<2; f++) {
+            for (int c=0; c<2; c++){
+                res[f][c]=num[f][c] * num[f][c];
+            }
+        }
+       for (int f=0; f<2; f++) {
+           for (int c=0; c<2; c++) {
+               System.out.print (res[f][c]+", ");
+           }
+           System.out.println();
+       }
     }
 }
