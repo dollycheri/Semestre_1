b3443b10960bd27a6f89b6ae33576a0e7356ff5e
dollycheri <146400981+dollycheri@users.noreply.github.com>
2023-11-23 18:00:48 -0600

    .

---
+++ Unidad_4/C#/Programa20_U4.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Programa20_U4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] num = { { 5, 6 }, { 8, 9 } };
+            int[,] res = new int[2, 2];
+
+            for (int f = 0; f < 2; f++)
+            {
+                for (int c = 0; c < 2; c++)
+                {
+                    res[f, c] = num[f, c] * num[f, c];
+                }
+            }
+
+            for (int f = 0; f < 2; f++)
+            {
+                for (int c = 0; c < 2; c++)
+                {
+                    Console.Write(res[f, c] + ", ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
