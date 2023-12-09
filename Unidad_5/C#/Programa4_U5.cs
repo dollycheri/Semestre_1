1d950ac7e77ad9acd5530af798c3be73ad4307ac
dollycheri <146400981+dollycheri@users.noreply.github.com>
2023-12-08 18:54:10 -0600

    .

---
+++ Unidad_5/C#/Programa4_U5.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Programa4_U5
+{
+    static void Main()
+    {
+        int numero = 24;
+
+        if (EsPrimo(numero))
+        {
+            Console.WriteLine(numero + " es un número primo.");
+        }
+        else
+        {
+            Console.WriteLine(numero + " no es un número primo.");
+        }
+    }
+    static bool EsPrimo(int numero)
+    {
+        if (numero <= 1)
+        {
+            return false;
+        }
+        for (int i = 2; i <= Math.Sqrt(numero); i++)
+        {
+            if (numero % i == 0)
+            {
+                return false; 
+            }
+        }
+        return true; 
+    }
+}
