6a6980308c81e96a6b440f4dd1822438d6e88736
dollycheri <146400981+dollycheri@users.noreply.github.com>
2023-12-08 20:01:13 -0600

    -

---
+++ Unidad_5/C#/Programa8_U5.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Programa8_U5
+{
+    static void Main()
+    {
+        int numero_Dados = 2; 
+        int resultado = Lanzar_Dados(numero_Dados);
+
+        Console.WriteLine("Resultado del lanzamiento: " + resultado);
+    }
+
+    static int Lanzar_Dados(int numero_Dados)
+    {
+        Random random = new Random();
+        int resultado = 0;
+
+        for (int i = 0; i < numero_Dados; i++)
+        {
+            int valor_Dado = random.Next(1, 7);
+            Console.WriteLine($"Dado {i + 1}: {valor_Dado}");
+
+            resultado += valor_Dado;
+        }
+
+        return resultado;
+    }
+}
