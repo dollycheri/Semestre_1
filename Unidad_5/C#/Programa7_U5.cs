93c3b26c7cc8b1d90417eda4b32ce7b8e91dee79
dollycheri <146400981+dollycheri@users.noreply.github.com>
2023-12-08 19:43:56 -0600

    -

---
+++ Unidad_5/C#/Programa7_U5.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Programa7_U5
+{
+    static void Main()
+    {
+        Console.WriteLine("Ingrese el subtotal de la factura:");
+        double subtotal = double.Parse(Console.ReadLine());
+
+        double iva = Calcular_IVA(subtotal);
+        double total = Calcular_Total(subtotal, iva);
+
+        Console.WriteLine($"Subtotal: ${subtotal}");
+        Console.WriteLine($"IVA (16%): ${iva}");
+        Console.WriteLine($"Total: ${total}");
+    }
+
+    static double Calcular_IVA(double subtotal)
+    {
+        double porcentaje_IVA = 0.16;
+        return subtotal * porcentaje_IVA;
+    }
+
+    static double Calcular_Total(double subtotal, double iva)
+    {
+        return subtotal + iva;
+    }
+}
